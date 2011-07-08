0cd8f2ca6abb4bbb6747040a37c3e2e59dc4cde6
Adam Creeger <acreeger@ticketfly.com>
2011-07-08 08:55:58 -0700

    Upping to 0.3.5.2

--- chrome_extension/js/injectScript.js
+++ chrome_extension/js/injectScript.js
@@ -1,1 +1,1 @@
-document.body.appendChild(document.createElement('script')).src=chrome.extension.getURL("js/turntable.fm.extend.js?v=0.3.5.1" + new Date().getTime());
+document.body.appendChild(document.createElement('script')).src=chrome.extension.getURL("js/turntable.fm.extend.js?v=0.3.5.2" + new Date().getTime());
