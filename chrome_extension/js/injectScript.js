aeacfd6b28f76b46aa8c6bbdde653cbfbaf67ab3
Mark Reeder <mark@arcticflare.com>
2011-07-12 23:04:26 -0600

    Tags clean-up, added the ability to close the tags tray, an All Songs link, etc.

--- chrome_extension/js/injectScript.js
+++ chrome_extension/js/injectScript.js
@@ -1,1 +1,1 @@
-document.body.appendChild(document.createElement('script')).src=chrome.extension.getURL("js/turntable.fm.extend.js?v=0.5.0.1" + new Date().getTime());
+document.body.appendChild(document.createElement('script')).src=chrome.extension.getURL("js/turntable.fm.extend.js?v=0.5.0.2" + new Date().getTime());
