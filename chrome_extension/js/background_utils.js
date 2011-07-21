3006c0b3b77d001ae7ada863743d72c76eb3dabe
Adam Creeger <acreeger@ticketfly.com>
2011-07-20 23:46:51 -0700

    Suggested songs "algorithm" now considers the most recent 20 songs, instead of 10

--- chrome_extension/js/background_utils.js
+++ chrome_extension/js/background_utils.js
@@ -8,15 +8,22 @@ Doing so will prevent the unit tests from running
 var TTExBGUtils = TTExBGUtils || {}
 
 TTExBGUtils.prepareSongLogForSimilarSongSearch = function(songLog) {
-	var numMostRecentSongsToConsider = 10
+
+	var minimumLength = 10
+	var numMostRecentSongsToConsider = 20
 	var minScore = 55
 
 	if (!songLog || songLog.length == 0) return [];
-
 	//chop off the last song, as it is currently playing
+
 	songLog.splice(songLog.length - 1,1);
 
-	if (songLog.length < numMostRecentSongsToConsider) return	[]
+	if (songLog.length < minimumLength) return []
+
+	//only keep a maximum of 10
+	if (songLog.length > numMostRecentSongsToConsider) {
+		songLog = songLog.slice(songLog.length - numMostRecentSongsToConsider)
+	}
 
 	var sortedSongLog = songLog.sort(function(a,b) {
 		if (a.score < b.score) return 1;
@@ -28,10 +35,5 @@ TTExBGUtils.prepareSongLogForSimilarSongSearch = function(songLog) {
 		return Math.round(element.score * 100) >= minScore
 	});
 
-	//only keep a maximum of 10
-	if (filteredSongLog.length > numMostRecentSongsToConsider) {
-		filteredSongLog = filteredSongLog.slice(filteredSongLog.length - numMostRecentSongsToConsider)
-	}
-	
 	return filteredSongLog
 }
--- chrome_extension_tests/BackgroundTests.js
+++ chrome_extension_tests/BackgroundTests.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+	var numMostRecentSongsToConsider = 20
+	var minLength = 10
+	var threshold = 55
+
 	module("Background_utils.js tests");
 
 	function verifyEmptyArray(array) {
@@ -14,7 +18,8 @@ $(document).ready(function() {
 	});
 
 	test("prepareSongLogForSimilarSongSearch: short log", function() {
-		var log = [1,2,3,4,5,6,7,8,9,10] // the last will get lopped off and 9 < the threshold of 10
+		var log = [/*1,2,3,4,5,6,7,8,9,10*/] // the last will get lopped off and 9 < the threshold of 10
+		for (var i = 1; i <= minLength; i++ ) log.push(i)
 		var result = TTExBGUtils.prepareSongLogForSimilarSongSearch(log)
 
 		verifyEmptyArray(result)
@@ -23,7 +28,9 @@ $(document).ready(function() {
 	test("prepareSongLogForSimilarSongSearch: results are sorted in descending score order", function() {
 		var songLog = [];
 
-		for (var i = 89; i <= 100;i++) { //it will chop off the last one
+		var start = 100 - minLength - 1
+
+		for (var i = start; i <= 100;i++) { //it will chop off the last one
 			songLog.push({score:i/100});
 		}
 
@@ -44,21 +51,26 @@ $(document).ready(function() {
 	test("prepareSongLogForSimilarSongSearch: only results above or equal 55% are included", function() {
 		var songLog = [];
 
-		for (var i = 49; i <= 60;i++) {
+		var expectedLength = 5
+
+		var end = threshold + 5
+		var start = end - minLength - 1
+		for (var i = start; i <= end;i++) {
 			songLog.push({score:i/100});
 		}
 
 		var result = TTExBGUtils.prepareSongLogForSimilarSongSearch(songLog)
 
 		ok(result,"result should have been not null")
-		equals(result.length,5,"Wrong number of results")
+		equals(result.length,expectedLength,"Wrong number of results")
 		equals(result[result.length-1].score,0.55,"The lowest entry should have been 0.55")
 	});
 
 	test("prepareSongLogForSimilarSongSearch: a log with all < 55% results in an empty array", function() {
 		var songLog = [];
 
-		for (var i = 10; i < 55;i = i + 2) {
+		var end = threshold - 1
+		for (var i = 10; i <= threshold;i = i + 2) {
 			songLog.push({score:i/100});
 		}
 
@@ -67,19 +79,38 @@ $(document).ready(function() {
 		verifyEmptyArray(result)
 	});
 
-	test("Only the ten most recent songs are kept", function() {
+	test("Only the ten most recent songs are kept, when 10 recent are lowest scoring", function() {
 		var songLog = [];
 
-		for (var i = 90; i >= 59; i--) {
+		var high = 90
+		var low = 59
+		for (var i = high; i >= low; i--) {
 			songLog.push({score:i/100});
 		}
 
 		var result = TTExBGUtils.prepareSongLogForSimilarSongSearch(songLog)
 
 		ok(result,"result should have been not null")
-		equals(result.length,10,"Wrong number of results")
-		equals(result[0].score,0.69)
-		equals(result[result.length-1].score,0.60)
+		equals(result.length,numMostRecentSongsToConsider,"Wrong number of results")
+		equals(result[0].score, (low + numMostRecentSongsToConsider) / 100)
+		equals(result[result.length-1].score,(low + 1)/100)
+	});
+
+	test("Only the ten most recent songs are kept, when 10 recent are highest scoring", function() {
+		var songLog = [];
+
+		var high = 90
+		var low = 59
+		for (var i = low; i <= high; i++) {
+			songLog.push({score:i/100});
+		}
+
+		var result = TTExBGUtils.prepareSongLogForSimilarSongSearch(songLog)
+
+		ok(result,"result should have been not null")
+		equals(result.length,numMostRecentSongsToConsider,"Wrong number of results")
+		equals(result[0].score,(high - 1)/100)
+		equals(result[result.length-1].score, (high - numMostRecentSongsToConsider) /100)
 	});
 
 
