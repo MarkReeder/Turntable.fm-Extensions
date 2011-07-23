16536434166be555f1c0e34da1708358f0d11202
Mark Reeder <mark@arcticflare.com>
2011-07-22 21:13:49 -0600

    Fix for CSS disappearing when extension is updated.

--- chrome_extension/js/injectScript.js
+++ chrome_extension/js/injectScript.js
@@ -1,1 +1,12 @@
-document.body.appendChild(document.createElement('script')).src=chrome.extension.getURL("js/turntable.fm.extend.js?v=0.6_" + new Date().getTime());
+(function() {
+    var js, css;
+    js = document.createElement('script');
+    js.src = chrome.extension.getURL("js/turntable.fm.extend.js?v=0.6_" + new Date().getTime());
+    document.body.appendChild(js);
+    
+    css = document.createElement('link');
+    css.rel = "stylesheet";
+    css.type = "text/css";
+    css.href=chrome.extension.getURL("css/turntable.fm.extend.css?v=0.6_" + new Date().getTime());
+    document.body.appendChild(css);
+})();
